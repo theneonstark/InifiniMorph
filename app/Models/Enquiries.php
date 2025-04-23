ccb4ce2acee2469d7e08325ac1a12e1eb37e24ff
theneonstark <mohd47149@gmail.com>
2025-04-23 15:38:17 +0530

    create and update date function in models

--- app/Models/Enquiries.php
+++ app/Models/Enquiries.php
@@ -7,4 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Enquiries extends Model
 {
     protected $fillable = ['fname', 'lname', 'email', 'phone', 'service', 'msg'];
+
+    public function getCreatedAtAttribute($value){
+        return date('d M y - h:i A', strtotime($value));
+    }
+
+    public function getUpdatedAtAttribute($value){
+        return date('d M y - h:i A', strtotime($value));
+    }
 }
