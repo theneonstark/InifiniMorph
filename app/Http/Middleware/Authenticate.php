01539b324d19e940c87900c53575e7cda18174bd
theneonstark <mohd47149@gmail.com>
2025-04-23 23:19:02 +0530

    add redirection

---
+++ app/Http/Middleware/Authenticate.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Auth\Middleware\Authenticate as Middleware;
+
+class Authenticate extends Middleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    protected function redirectTo(Request $request)
+    {
+        if(!$request->expectsJson()){
+            return route('mylogin');
+        }
+    }
+}
